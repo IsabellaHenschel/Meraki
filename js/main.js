4be35ec07c80f46313122499b6bbb4ff02a7c77a
IsabellaHenschel <69045047+IsabellaHenschel@users.noreply.github.com>
2021-06-14 11:12:07 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+jQuery(document).ready(function($){
 
+    window.onscroll = function(){
+        if(window.pageYOffset > 50){
+            $('#header').addClass("fixar");
+    } else{
+        $('#header').removeClass("fixar");
+    }
+    };
+});
