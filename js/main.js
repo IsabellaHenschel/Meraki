d95319956c2cc469245b36e1ae48ce67d9d2a52c
IsabellaHenschel <69045047+IsabellaHenschel@users.noreply.github.com>
2021-10-01 19:26:42 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,37 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
 
-    window.onscroll = function(){
-        if(window.pageYOffset > 50){
+    window.onscroll = function () {
+        if (window.pageYOffset > 50) {
             $('#header').addClass("fixar");
-    } else{
-        $('#header').removeClass("fixar");
-    }
+        } else {
+            $('#header').removeClass("fixar");
+        }
     };
+
+    $(function () {
+        $.ajax({
+            url: 'http://localhost:5000/listar_livros',
+            method: 'GET',
+            dataType: 'json',
+            success: listar,
+            error: function () {
+                alert("Erro ao ler dados, verifique o backend");
+            }
+        });
+
+        function listar(livros) {
+            for (var i in livros) {
+                lin = '<tr>' +
+                    '<td>' + livros[i].titulo + '</td>' +
+                    '<td>' + livros[i].autor + '</td>' +
+                    '<td>' + livros[i].editora + '</td>' +
+                    '<td>' + livros[i].genero + '</td>' +
+                    '<td>' + livros[i].isbn + '</td>' +
+                    '</tr>';
+
+                $('#corpoTabelaLivros').append(lin)
+            }
+        };
+
+    });
 });
